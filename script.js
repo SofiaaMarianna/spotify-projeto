af618bc734c4efc8d8d92be4ecc48797d16b8c36
Sofia Marianna <83606120+SofiaaMarianna@users.noreply.github.com>
2024-01-26 14:04:16 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const greetingElement = document.getElementById("greeting");
+
+const currentHour = new Date().getHours();
+
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
